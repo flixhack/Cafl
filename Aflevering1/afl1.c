36dc25244c9133e1aeb815bc05cde00be36cec13
flixhack <felix.benjamin.mathiassen@gmail.com>
2021-09-11 18:53:51 +0200

    working v1

---
+++ Aflevering1/afl1.c
@@ -0,0 +1,32 @@
+//opt. Ikke out uge, dage, osv. hvis tallet er lig med 0
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void){
+    int isec,
+    rest,
+    week,
+    day,
+    hour,
+    minute,
+    second;
+
+    printf("Enter seconds: ");
+    scanf("%i", &isec);
+
+    week = isec / 604800;
+    rest = isec % 604800;
+    day = rest / 86400;
+    rest = rest % 86400;
+    hour = rest / 3600;
+    rest = rest % 3600;
+    minute = rest / 60;
+    rest = rest % 60;
+    second = rest;
+
+    printf("\n%s%i%s%i%s%i%s%i%s%i", "Weeks: ", week, " Days: ", day, " Hours: ", hour, " Minuts: ", minute, " Seconds: ", second);
+
+    return 0;
+
+}
